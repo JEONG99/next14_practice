1f508762259b8c374a9d6e33e797451654b27911
jeong99 <jbh990621@gmail.com>
2024-03-11 16:23:40 +0900

    feat: 홈화면 getMovies

---
+++ app/(home)/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const data = await getMovies();
+
+  return <div>{JSON.stringify(data)}</div>;
+}
