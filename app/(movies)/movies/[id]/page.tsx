2b0a20c009aadeba7eb31b10231fe8d1add4a2c1
jeong99 <jbh990621@gmail.com>
2024-03-11 17:38:12 +0900

    feat: movie detail 화면 suspense 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,20 @@
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
 export default function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Movie info loading...</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Movie videos loading...</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
